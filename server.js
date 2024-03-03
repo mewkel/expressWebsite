dc24fce3de8f51112554e14a8adff5de578c321f
Mikkel Brun Larsen <mikkel@Mikkel.Mikkel>
2024-03-03 14:42:56 +0100

    Added something

--- server.js
+++ server.js
@@ -1,37 +1,19 @@
-
 const express = require("express");
-const mongoose = require("mongoose");
 
 const app = express();
 app.use(express.static("public"));
 
-mongoose.connect(
-	"mongodb+srv://testUser:1234@cluster0.ypocggm.mongodb.net/Database?retryWrites=true&w=majority&appName=Cluster0",
-);
+app.get("/", () => {
 
-const Schema = mongoose.Schema;
-
-const UserCount = mongoose.model(
-	"UserCount",
-	new Schema({
-		Count: String,
-	}),
-	"UserCount",
-);
-
-app.get("/", (req, res) => {
-
-	const new_UserCount = new UserCount({
-
-		Count: "1",
-
-
-	}).save();
+  console.log("test");
 
 });
 
-const server_port = 3000;
+app.post("/", () => {
+});
 
-const server = app.listen(server_port, () => {
-	console.log("Server running on http://127.0.0.1:" + server_port);
+const port = 3000;
+
+const server = app.listen(port, () => {
+  console.log("Server running on http://127.0.0.1:" + port);
 });
